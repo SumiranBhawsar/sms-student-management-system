354ce99dcdfc42da56fa8325f189ac2849d7c406
SumiranBhawsar <sumiranbhawsar149@gmail.com>
2025-07-22 13:21:05 +0530

    config the prettier in the project to alignment

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -2,10 +2,10 @@ import SignInPageContent from "@/components/auth/SignInPageContent";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Sign In - Student Management System",
-  description: "Sign in to the EduSys portal.",
+    title: "Sign In - Student Management System",
+    description: "Sign in to the EduSys portal.",
 };
 
 export default function SignInPage() {
-  return <SignInPageContent />;
+    return <SignInPageContent />;
 }
