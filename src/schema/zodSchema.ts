c8402f0075ada979e3758a98c96cc7a6077bb321
SumiranBhawsar <sumiranbhawsar149@gmail.com>
2025-07-22 13:41:56 +0530

    change

--- src/schema/zodSchema.ts
+++ src/schema/zodSchema.ts
@@ -4,7 +4,7 @@ export const signinSchema = z.object({
     email: z.string().email({ message: "Please enter a valid email address." }),
     password: z
         .string()
-        .min(6, { message: "Password must be at least 6 characters long." }),
+        .min(6, { message: "Password must be at least 6(six) characters long." }),
 });
 
 export type SigninFormType = z.infer<typeof signinSchema>;
